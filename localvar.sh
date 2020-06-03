7749532266a027b20383d8e2c7532c7b212ff70a
atsushi-suzuki <atsushi.suzuki.dev@gmail.com>
2020-06-03 23:08:48 +0900

    add local var test shells

---
+++ localvar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+func1 ()
+{
+	echo "func1: message = $message"
+	local message="Guten tag"
+}
+
+message="Hello"
+echo "main(1st): message = $message"
+func1
+echo "main(2nd): message = $message"
+
