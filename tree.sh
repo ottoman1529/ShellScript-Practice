3cb575a0e9dda8aa7d3a5c0afe53d20fa49b812f
atsushi-suzuki <atsushi.suzuki.dev@gmail.com>
2020-06-03 23:22:26 +0900

    add indent

--- tree.sh
+++ tree.sh
@@ -4,8 +4,11 @@ list_recursive ()
 {
 	# ローカル変数で宣言
 	local filepath=$1
+	local indent=$2
 
-	echo "$filepath"
+	# インデント付きで、パス部分を取り除いてファイル名を表示する
+	# bashのマッチ機能. ${変数名##パターン} = 最長マッチでパターン前方一致した部分を取り除く
+	echo "${indent}${filepath##*/}"
 
 	if [ -d "$filepath" ]; then
 		# ディレクトリである場合、その中に含まれるファイルや
@@ -13,10 +16,10 @@ list_recursive ()
 		local fname
 		for fname in $(ls "$filepath")
 		do
-			#ディレクトリ内のファイルを表示
-			list_recursive "${filepath}/${fname}"
+			#インデントにスペースを追加して再帰呼び出し
+			list_recursive "${filepath}/${fname}" "    $indent"
 		done
 	fi
 }
 
-list_recursive "$1"
+list_recursive "$1" ""
