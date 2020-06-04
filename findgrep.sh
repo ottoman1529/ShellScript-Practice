07184995cf4f5233789609c81b9447f0308f32db
atsushi-suzuki <atsushi.suzuki.dev@gmail.com>
2020-06-04 20:05:15 +0900

    第三引数でファイル名のパターンを指定できる

--- findgrep.sh
+++ findgrep.sh
@@ -2,12 +2,19 @@
 
 pattern=$1
 directory=$2
+name=$3
 
-# 第二引数(起点ディレクトリ)がから文字列ならば、デフォルト値として
+# 第2引数(起点ディレクトリ)が空文字列ならば、デフォルト値として
 # . (カレントディレクトリ)を設定
 if [ -z "$directory" ]; then
 	directory='.'
 fi
 
-find . -type f | xargs grep -nH "$pattern"
+# 第3引数(検索ファイルパターン)が空文字列ならば、
+# デフォルト値として'*'を設定
+if [ -z "$name" ]; then
+	name='*'
+fi
+
+find . -type f -name "$name" | xargs grep -nH "$pattern"
 
