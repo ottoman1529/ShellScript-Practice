072355a96257682e6da760780d577dc2972ba9f8
atsushi-suzuki <atsushi.suzuki.dev@gmail.com>
2020-06-04 20:02:13 +0900

    第二引数で検索ディレクトリを設定

--- findgrep.sh
+++ findgrep.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 pattern=$1
+directory=$2
+
+# 第二引数(起点ディレクトリ)がから文字列ならば、デフォルト値として
+# . (カレントディレクトリ)を設定
+if [ -z "$directory" ]; then
+	directory='.'
+fi
+
 find . -type f | xargs grep -nH "$pattern"
 
