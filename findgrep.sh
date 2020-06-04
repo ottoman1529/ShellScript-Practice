e8fa3b757286809dff9be25360a59f4123fac189
atsushi-suzuki <atsushi.suzuki.dev@gmail.com>
2020-06-04 20:12:22 +0900

    ヘルプドキュメントの追加

--- findgrep.sh
+++ findgrep.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+usage()
+{
+	# シェルスクリプトのファイル名を取得
+	local script_name=$(basename "$0")
+
+	# ヒアドキュメントでヘルプを表示
+	cat << END
+Usage: $script_name PATTERN [PATH] [NAME_PATTERN]
+Find file in current directory recursively, and print lines which match PATTERN.
+
+	PATH		find file in PATH directory, instead of current direcotry
+	NAME_PATTERN	specify name pattern to find file
+
+Examples:
+	$script_name return
+	$script_name return ~ '*.txt'
+END
+}
+
+# コマンドライン引数が0個のとき(何も指定されないとき)
+if [ "$#" -eq 0 ]; then
+	usage
+	exit 1
+fi
+
 pattern=$1
 directory=$2
 name=$3
